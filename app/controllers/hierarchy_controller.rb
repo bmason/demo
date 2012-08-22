4503e21121af5d2500d705c10e7b601e3e4c6568
Brian Mason <bmason@itassociates.com>
2012-08-22 23:22:27 +0700

    Heroku_2

--- app/controllers/hierarchy_controller.rb
+++ app/controllers/hierarchy_controller.rb
@@ -2,7 +2,7 @@
 
 class HierarchyController < ApplicationController
   def hierarchy
-    load 'sub.rb'
+    load 'allsub.rb'
 #    return render :text => "session info #{session.inspect}"
   end
 end
